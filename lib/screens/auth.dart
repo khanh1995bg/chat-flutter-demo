4b85d215447e193fecfb2c6f15a009b4101ba423
Khánh Nguyễn Duy <>
2023-10-23 17:00:31 +0700

    create project

---
+++ lib/screens/auth.dart
@@ -0,0 +1,194 @@
+import 'dart:io';
+
+import 'package:chat_app_flutter/widget/user_image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+
+final _firebase = FirebaseAuth.instance;
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final _form = GlobalKey<FormState>();
+  bool _isLogin = true;
+  String _enteredEmail = '';
+  String _enteredPassword = '';
+  String _enteredUsername = '';
+  File? _selectedImage;
+  bool _isAuthenticating = false;
+
+  void _onSubmit() async {
+    final bool isValid = _form.currentState!.validate();
+    if (!isValid || !_isLogin && _selectedImage == null) return;
+
+    _form.currentState!.save();
+
+    try {
+      setState(() {
+        _isAuthenticating = true;
+      });
+      if (_isLogin) {
+        await _firebase.signInWithEmailAndPassword(
+            email: _enteredEmail, password: _enteredPassword);
+      } else {
+        final userCredentials = await _firebase.createUserWithEmailAndPassword(
+          email: _enteredEmail,
+          password: _enteredPassword,
+        );
+
+        // create storage save image
+        final storageRef = FirebaseStorage.instance
+            .ref()
+            .child('user_images')
+            .child('${userCredentials.user!.uid}.jpg');
+
+        await storageRef.putFile(_selectedImage!);
+        final imageUrl = await storageRef.getDownloadURL();
+
+        // action store firebase
+        FirebaseFirestore.instance
+            .collection('users')
+            .doc(userCredentials.user!.uid)
+            .set({
+          'username': _enteredUsername,
+          'email': _enteredEmail,
+          'image_url': imageUrl,
+        });
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'email-already-in-use') {}
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(e.message ?? 'Authentication failed')));
+
+      setState(() {
+        _isAuthenticating = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+          child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(
+                top: 30,
+                bottom: 20,
+                left: 20,
+                right: 20,
+              ),
+              width: 200,
+              child: Image.asset('assets/images/logo.png'),
+            ),
+            Card(
+              margin: const EdgeInsets.all(20),
+              child: SingleChildScrollView(
+                  child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Form(
+                    key: _form,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        if (!_isLogin)
+                          UserImagePicker(
+                            onPickImage: (pickedImage) {
+                              _selectedImage = pickedImage;
+                            },
+                          ),
+                        TextFormField(
+                          decoration:
+                              const InputDecoration(labelText: 'Email Address'),
+                          keyboardType: TextInputType.emailAddress,
+                          autocorrect: false, //xoá tự động chỉnh sửa text
+                          textCapitalization: TextCapitalization
+                              .none, // bỏ chữ viết hoa đầu tiên
+                          validator: (value) {
+                            if (value == null ||
+                                value.trim().isEmpty ||
+                                !value.contains('@')) {
+                              return 'Please enter a valid email address.';
+                            }
+                            return null;
+                          },
+                          onSaved: (newValue) {
+                            _enteredEmail = newValue!;
+                          },
+                        ),
+                        if (!_isLogin)
+                          TextFormField(
+                            decoration:
+                                const InputDecoration(labelText: 'User name'),
+                            enableSuggestions: false, // bỏ gợi ý
+                            validator: (value) {
+                              if (value == null ||
+                                  value.isEmpty ||
+                                  value.trim().length < 4) {
+                                return 'Please enter at least 4 characters';
+                              }
+                              return null;
+                            },
+                            onSaved: (newValue) {
+                              _enteredUsername = newValue!;
+                            },
+                          ),
+                        TextFormField(
+                          decoration:
+                              const InputDecoration(labelText: 'Password'),
+                          obscureText: true, //ẩn text
+                          validator: (value) {
+                            if (value == null || value.trim().length < 6) {
+                              return 'Password must be ar least 6 characters long.';
+                            }
+                            return null;
+                          },
+                          onSaved: (newValue) {
+                            _enteredPassword = newValue!;
+                          },
+                        ),
+                        const SizedBox(height: 12),
+                        if (!_isAuthenticating)
+                          ElevatedButton(
+                            onPressed: _onSubmit,
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Theme.of(context)
+                                    .colorScheme
+                                    .primaryContainer),
+                            child: Text(_isLogin ? 'Login' : 'Sign up'),
+                          ),
+                        if (_isAuthenticating)
+                          const CircularProgressIndicator(),
+                        if (!_isAuthenticating)
+                          TextButton(
+                            onPressed: (() {
+                              setState(() {
+                                _isLogin = !_isLogin;
+                              });
+                            }),
+                            child: Text(_isLogin
+                                ? "Create an account"
+                                : 'I already have an account'),
+                          ),
+                      ],
+                    )),
+              )),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
---
+++ lib/screens/chat.dart
@@ -0,0 +1,32 @@
+import 'package:chat_app_flutter/widget/chat_messages.dart';
+import 'package:chat_app_flutter/widget/new_message.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Chat'),
+          actions: [
+            IconButton(
+                onPressed: (() {
+                  FirebaseAuth.instance.signOut();
+                }),
+                icon: Icon(
+                  Icons.exit_to_app,
+                  color: Theme.of(context).colorScheme.primary,
+                ))
+          ],
+        ),
+        body: Column(
+          children: const [
+            Expanded(child: ChatMessages()),
+            NewMessage(),
+          ],
+        ));
+  }
+}
